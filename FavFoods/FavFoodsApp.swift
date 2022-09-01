ed1dbc92d09d8e1a91c2ddf3c964f8c2ee33817f
Andi Rustianto <anditodev46@gmail.com>
2022-09-01 14:49:29 +0700

    Init project

---
+++ FavFoods/FavFoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavFoodsApp.swift
+//  FavFoods
+//
+//  Created by Armstrong on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct FavFoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
